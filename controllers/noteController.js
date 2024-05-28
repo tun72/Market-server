1b01becbe3cd7d1cca92694d9a2c2dbbb72f7048
Tun72 <tunt72553@gmail.com>
2024-05-28 14:48:42 +0630

    first commit

---
+++ controllers/noteController.js
@@ -0,0 +1,9 @@
+/* NOTE CONTROLLER */
+
+// get method
+exports.getNotes = (req, res, next) => {
+  return res.status(200).json({});
+};
+
+// post method
+exports.postNote = (req, res, next) => {};
