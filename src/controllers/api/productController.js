747fab425d40bb8a7315ff7d4c576ebd4e31ef3f
Tun Tun Myint <tunt72553@gmail.com>
2025-07-13 23:22:04 +0630

    add featured product

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -1,5 +1,3 @@
-
-const { body } = require("express-validator");
 const { Product, Type } = require("../../models/productModel");
 
 const factory = require("../handlerFactory");
@@ -7,6 +5,7 @@ const catchAsync = require("../../utils/catchAsync");
 const { Category } = require("../../models/productModel");
 const { Event } = require("../../models/eventsModel");
 const AppError = require("../../utils/appError");
+const mongoose = require("mongoose")
 
 // products
 exports.getAllProducts = factory.getAll({
@@ -14,11 +13,233 @@ exports.getAllProducts = factory.getAll({
     fields: ["brand", "category", "tags", "merchant", "type"],
 })
 
-exports.getProductById = factory.getOne({
-    Model: Product,
-    fields: ["brand", "category", "tags", "merchant", "type"]
+// exports. = factory.getOne({
+//     Model: Product,
+//     fields: ["brand", "category", "tags", "merchant", "type"]
 
+// })
+
+exports.getProductById = catchAsync(async (req, res, next) => {
+    const productId = req.params.id;
+    if (!productId) {
+        return next(new AppError("Type Id is required.", 400));
+    }
+
+    if (!mongoose.isValidObjectId(productId)) {
+        return next(new AppError("Is not valid ID", 404));
+    }
+
+
+
+    const products = await Product.aggregate([
+        // Match main product
+        { $match: { _id: new mongoose.Types.ObjectId(productId) } },
+
+        {
+            $lookup: {
+                from: "tags",
+                localField: "tags",
+                foreignField: "_id",
+                as: "tags"
+            }
+        },
+
+        // Populate category
+        {
+            $lookup: {
+                from: "categories",
+                localField: "category",
+                foreignField: "_id",
+                as: "category"
+            }
+        },
+        { $unwind: { path: "$category", preserveNullAndEmptyArrays: true } },
+
+        // Populate type
+        {
+            $lookup: {
+                from: "types",
+                localField: "type",
+                foreignField: "_id",
+                as: "type"
+            }
+        },
+        { $unwind: { path: "$type", preserveNullAndEmptyArrays: true } },
+
+
+        // Populate merchant
+        {
+            $lookup: {
+                from: "users",
+                localField: "merchant",
+                foreignField: "_id",
+                as: "merchant"
+            }
+        },
+        { $unwind: { path: "$merchant", preserveNullAndEmptyArrays: true } },
+
+        {
+            $addFields: {
+                "merchant.address.fulladdress": {
+                    $cond: {
+                        if: { $and: ["$merchant", "$merchant.address"] },
+                        then: {
+                            $concat: [
+                                { $ifNull: ["$merchant.address.street", ""] },
+                                ", ",
+                                { $ifNull: ["$merchant.address.city", ""] },
+                                ", ",
+                                { $ifNull: ["$merchant.address.state", ""] },
+                                ", ",
+                                { $ifNull: ["$merchant.address.country", ""] },
+
+                            ]
+                        },
+                        else: null
+                    }
+                },
+                "merchant.id": "$_id"
+            }
+        },
+
+
+        // Get related products 
+        {
+            $lookup: {
+                from: "products",
+                let: {
+                    categoryId: "$category._id",
+                    currentId: "$_id"
+                },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $and: [
+                                    { $eq: ["$category", "$$categoryId"] },
+                                    { $ne: ["$_id", "$$currentId"] }
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: "tags",
+                            localField: "tags",
+                            foreignField: "_id",
+                            as: "tags"
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: "categories",
+                            localField: "category",
+                            foreignField: "_id",
+                            as: "category"
+                        }
+                    },
+                    { $unwind: { path: "$category", preserveNullAndEmptyArrays: true } },
+
+                    // Populate type
+                    {
+                        $lookup: {
+                            from: "types",
+                            localField: "type",
+                            foreignField: "_id",
+                            as: "type"
+                        }
+                    },
+                    { $unwind: { path: "$type", preserveNullAndEmptyArrays: true } },
+
+                    // Populate merchant
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "merchant",
+                            foreignField: "_id",
+                            as: "merchant"
+                        }
+                    },
+                    { $unwind: { path: "$merchant", preserveNullAndEmptyArrays: true } },
+                    { $limit: 5 },
+                    {
+                        $addFields: {
+                            "merchant.address.fulladdress": {
+                                $cond: {
+                                    if: { $and: ["$merchant", "$merchant.address"] },
+                                    then: {
+                                        $concat: [
+                                            { $ifNull: ["$merchant.address.street", ""] },
+                                            ", ",
+                                            { $ifNull: ["$merchant.address.city", ""] },
+                                            ", ",
+                                            { $ifNull: ["$merchant.address.state", ""] },
+                                            ", ",
+                                            { $ifNull: ["$merchant.address.country", ""] },
+
+                                        ]
+                                    },
+                                    else: null
+                                }
+                            },
+                            "merchant.id": "$_id"
+                        }
+                    },
+                    {
+                        $project: {
+                            __v: 0,
+                            _id: 0,
+                            merchant: {
+                                _id: 0,
+                                "password": 0,
+                                "randToken": 0,
+                                "NRCNumber": 0,
+                                "NRCFront": 0,
+                                "NRCBack": 0,
+                                "balance": 0,
+                                "__v": 0,
+                                "createdAt": 0,
+                                "updatedAt": 0,
+                                role: 0
+                            }
+                        }
+                    }  // Exclude unnecessary fields
+                ],
+                as: "relatedProducts"
+            }
+        },
+        {
+            $project: {
+                __v: 0,
+                _id: 0,
+                merchant: {
+                    _id: 0,
+                    "password": 0,
+                    "randToken": 0,
+                    "NRCNumber": 0,
+                    "NRCFront": 0,
+                    "NRCBack": 0,
+                    "balance": 0,
+                    "__v": 0,
+                    "createdAt": 0,
+                    "updatedAt": 0,
+                    role: 0
+                }
+            }
+        },
+
+
+        // Convert to single document
+        { $limit: 1 }
+    ]);
+
+
+    if (!products.length) {
+        return next(new AppError("Product Not found with that Id", 404));
+    }
+    res.status(200).json({ message: "success", product: products[0] })
 })
+
 
 exports.updateProduct = factory.updateOne(Product)
 exports.removeProduct = factory.deleteOne(Product)
@@ -31,8 +252,43 @@ exports.getFeaturedProducts = catchAsync(async (req, res, next) => {
         { $match: { isFeatured: true } },
         { $sample: { size: 5 } },
         { $addFields: { id: "$_id" } },
-        { $project: { _id: 0, __v: 0 } }
+        {
+            $lookup: {
+                from: "users",           // collection to join
+                localField: "merchant",    // field in Post
+                foreignField: "_id",     // field in User
+                as: "merchant"      // output array field
+            },
 
+        },
+        {
+            $unwind: {
+                path: "$merchant",
+                preserveNullAndEmptyArrays: true // Optional: if some products have no merchant
+            }
+        }, {
+            $project: {
+                __v: 0,
+                _id: 0,
+                merchant: {
+                    _id: 0,
+                    "password": 0,
+                    "randToken": 0,
+                    "phone": 0,
+                    "address": 0,
+                    "description": 0,
+                    "NRCNumber": 0,
+                    "NRCFront": 0,
+                    "NRCBack": 0,
+                    "balance": 0,
+                    "rating": 0,
+                    "__v": 0,
+                    "createdAt": 0,
+                    "updatedAt": 0,
+                    role: 0
+                }
+            }
+        }
     ])
 
     return res.status(200).json({ message: "sucess", products })
@@ -69,10 +325,7 @@ exports.getRelatedProduct = catchAsync(async (req, res, next) => {
     }
 
     const products = await Product.aggregate([
-        // { $match: { type: typeId } },
-        { $sample: { size: 5 } },
-        { $addFields: { id: "$_id" } },
-        { $project: { _id: 0, __v: 0 } }
+        { $match: { _id: productId } },
     ])
     res.status(200).json({ message: "success", products })
 })
