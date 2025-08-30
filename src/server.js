04a367ae36ccbff6be8790fcf46bca49f4c51be4
Tun Tun Myint <tunt72553@gmail.com>
2025-08-30 22:43:08 +0630

    update whitelist

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(express.json()).use(cookieParser());
 app.use(express.static("public"));
 app.use(express.static("uploads"));
 
-let whitelist = ["http://localhost:5173", "http://localhost:5174", "https://ayeyarmart.studentactivities.online", "http://150.95.81.76:5173"]
+let whitelist = ["http://localhost:5173", "http://localhost:5174", "https://ayeyarmart.studentactivities.online", "http://150.95.81.76:5173", "https://ayeyar-merchant.vercel.app"]
 const corsOptions = {
   origin: function (
     origin,
