5887d4f77dc9172e14f3083a52a7afe3663ed039
Tun Tun Myint <tunt72553@gmail.com>
2025-08-30 19:30:40 +0630

    fix api issues

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -13,15 +13,15 @@ const router = express.Router()
 // const categoryRoute = require("./routes/categoryRoutes")
 // routes
 // app.use("/api/v1/products", productRoute);
-router.use("/api/v1/auth", authRoute);
+router.use("/v1/auth", authRoute);
 
-router.use("/api/v1/seller", authMiddleware, authorise(true, "seller"), sellerRoute);
+router.use("/v1/seller", authMiddleware, authorise(true, "seller"), sellerRoute);
 // router.use("/api/v1/categories", categoryRoute);
 
-router.use("/api/v1/admin", authMiddleware, authorise(true, "admin"), adminRoute);
+router.use("/v1/admin", authMiddleware, authorise(true, "admin"), adminRoute);
 
-router.use("/api/v1/user", userRoute)
+router.use("/v1/user", userRoute)
 
-router.use("/api/v1/message", authMiddleware, mesageRoute)
+router.use("/v1/message", authMiddleware, mesageRoute)
 
 module.exports = router
